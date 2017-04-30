689a62bd0f499b20f4c701dcc8cfdada07fd1f82
Yusinto Ngadiman <yus@Yusintos-MacBook-Pro.local>
2017-04-30 11:45:53 +1000

    Started implementing merchant details screen. TODO: footer buttons, back navigation, display dynamic merchant details, credit card logo.

--- src/screens/merchant/merchantDetails.js
+++ src/screens/merchant/merchantDetails.js
@@ -11,34 +11,17 @@ import {colors} from '../../styles';
 import FontAwesomeIcon from '../../../node_modules/react-native-vector-icons/FontAwesome';
 import MERCHANTS from '../../data/merchants.json';
 
-const BackNavigation = (props) =>
-  <TouchableOpacity onPress={() => props.goBack()}>
-    <FontAwesomeIcon style={{position: 'absolute', left: 13}} name="chevron-left" size={20} color="#FFFFFF"/>
-  </TouchableOpacity>;
+//const BackNavigation = (props) =>
+//  <TouchableOpacity onPress={() => props.goBack()}>
+//    <FontAwesomeIcon style={{position: 'absolute', left: 13}} name="chevron-left" size={20} color="#FFFFFF"/>
+//  </TouchableOpacity>;
 
 export default class MerchantDetails extends Component {
-  static navigationOptions = ({ navigation }) => {
-    const {state, setParams} = navigation;
-    const isInfo = state.params.mode === 'info';
-    const {user} = state.params;
-
-    return {
-      title: '',
-      headerBackTitle: null,
-      //header: {
-      //  visible: true,
-      //  //left: null,
-      //  style: {
-      //    backgroundColor: colors.primary,
-      //  },
-      //  titleStyle: {
-      //    color: colors.white,
-      //    fontSize: 16,
-      //    fontWeight: '300',
-      //  },
-      //  tintColor: colors.white,
-      //}
-    }
+  static navigationOptions = {
+    title: '',
+    header: {
+      visible: false,
+    },
   };
 
   render() {
@@ -49,7 +32,34 @@ export default class MerchantDetails extends Component {
 
     return (
       <View style={styles.root}>
-        <Text>{merchant.key}</Text>
+        <Image style={styles.merchantImage} source={{uri: merchant.imageUrl}}/>
+        <View style={styles.merchantDetailsContainer}>
+          <Text style={styles.merchantTitle}>{merchant.title}</Text>
+          <Text style={styles.merchantAddress}>{merchant.description}</Text>
+        </View>
+        <View style={styles.requestSummaryContainer}>
+          <Text style={styles.summary}>Summary</Text>
+          <View style={styles.summaryItemContainer}>
+            <Text style={styles.requestedCash}>Requested Cash</Text>
+            <Text style={styles.requestedAmount}>SGD 30.00</Text>
+          </View>
+          <View style={styles.summaryItemContainer}>
+            <Text style={styles.label}>Service Fee</Text>
+            <Text style={styles.amount}>SGD 2.10</Text>
+          </View>
+          <View style={styles.summaryItemContainer}>
+            <Text style={styles.label}>Total Payment</Text>
+            <Text style={styles.amount}>SGD 32.10</Text>
+          </View>
+          <View style={styles.summaryItemContainer}>
+            <Text style={styles.label}>Charged to</Text>
+            <Text style={styles.amount}>****8884</Text>
+          </View>
+        </View>
+        <Text style={styles.footerText}>
+          You will only be charged after you have picked up
+          the cash from this merchant
+        </Text>
       </View>
     );
   }
@@ -60,11 +70,76 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: colors.primary,
+    backgroundColor: colors.background,
     width: '100%',
   },
-  fbLogo: {
-    position: 'absolute',
-    left: 20,
+  merchantImage: {
+    height: 150,
+    width: '100%',
+    alignSelf: 'center',
+    marginTop: 0,
   },
+  merchantDetailsContainer: {
+    height: 100,
+    width: '100%',
+    backgroundColor: colors.white,
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border,
+  },
+  merchantTitle: {
+    fontSize: 18,
+    color: colors.font,
+  },
+  merchantAddress: {
+    marginTop: 10,
+    fontSize: 12,
+    color: colors.secondaryFont,
+  },
+  requestSummaryContainer: {
+    marginTop: 8,
+    height: 200,
+    width: '100%',
+    backgroundColor: colors.white,
+    padding: 25,
+    borderTopWidth: 1,
+    borderTopColor: colors.border,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border,
+  },
+  summary: {
+    fontSize: 10,
+    color: colors.font,
+  },
+  summaryItemContainer: {
+    flexGrow: 0.1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border,
+  },
+  requestedCash: {
+    fontSize: 14,
+    color: colors.font,
+  },
+  requestedAmount: {
+    fontSize: 18,
+    color: colors.primaryDark,
+  },
+  label: {
+    fontSize: 12,
+    color: colors.tertiaryFont,
+  },
+  amount: {
+    fontSize: 12,
+    color: colors.font,
+  },
+  footerText: {
+    marginTop: 10,
+    fontSize: 10,
+    color: colors.font,
+    textAlign: 'center',
+  }
 });
--- src/styles.js
+++ src/styles.js
@@ -4,7 +4,11 @@ export const colors = {
   primaryDark: '#00A76D',
   primaryLight: '#00A66C',
   white: '#FFFFFF',
+  background: '#F5F5F5',
   black: '#000000',
   font: '#383838',
+  secondaryFont: '#6D6D6D',
+  tertiaryFont: '#9B9B9B',
   placeholder: '#97DAC2',
+  border: '#E2E2E2',
 };
